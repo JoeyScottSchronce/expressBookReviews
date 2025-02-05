fd0d19e5cb06e6367189f164112081f9efea0af1
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-02-05 16:32:40 -0500

    added Promise and setTimeout functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -20,51 +19,65 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
+public_users.get('/', function (req, res) {
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(books);
+        }, 2000); //setTimeout waits 2 seconds before resolving the request
+    })
+    .then((books) => {
+        res.status(200).send(JSON.stringify(books, null, 4));
+    });
 });
-
+  
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  let ISBN = req.params.isbn;
-  if (books[ISBN]) {
-    return res.status(200).send(books[ISBN])
-  } else {
-    return res.status(404).send("book not found.")
-  }
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  let author = req.params.author;
+    let ISBN = req.params.isbn;
 
-  if (author) {
-    let filtered = [];
-    for (key in books) {
-        if (books[key].author.toLowerCase() === author.toLowerCase()) {
-            filtered.push(books[key]);
-        }
-    } res.status(200).send(filtered);
-  } else {
-    res.status(404).send("Author not found!");
-  }
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (books[ISBN]) {
+                resolve(books[ISBN]);
+            } else {
+                reject("book not found.");
+            }
+        }, 2000);
+    })
+    .then((details) => {
+        res.status(200).send(details);
+    })
+    .catch((error) => {
+        res.status(404).send(error);
+    });
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    let title = req.params.title;
+// Get book details based on author with a delay of two seconds
+public_users.get('/author/:author', function (req, res) {
+    let author = req.params.author.toLowerCase();
 
-    if (title) {
-      let filtered = [];
-      for (key in books) {
-          if (books[key].title.toLowerCase() === title.toLowerCase()) {
-              filtered.push(books[key]);
-          }
-      } res.status(200).send(filtered);
-    } else {
-      res.status(404).send("Title not found!");
-    }
-  });
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let filtered = Object.values(books).filter(book => book.author.toLowerCase() === author);
+            filtered.length ? resolve(filtered) : reject("Author not found!");
+        }, 2000);
+    })
+    .then(filtered => res.status(200).send(filtered))
+    .catch(error => res.status(404).send(error));
+});
+
+// Get book details based on title with a delay of two seconds
+public_users.get('/title/:title', function (req, res) {
+    let title = req.params.title.toLowerCase();
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let filtered = Object.values(books).filter(book => book.title.toLowerCase() === title);
+            filtered.length ? resolve(filtered) : reject("title not found!");
+        }, 2000);
+    })
+    .then(filtered => res.status(200).send(filtered))
+    .catch(error => res.status(404).send(error));
+});
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
